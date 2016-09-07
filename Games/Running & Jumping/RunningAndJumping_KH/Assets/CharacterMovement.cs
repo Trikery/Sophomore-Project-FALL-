0ca10837897ce0fb0213d75b417192be292c1e6d
Kirt Hullinger <kirt.jh@gmail.com>
2016-09-07 08:45:36 -0600

    Initialize Game (Running & Jumping) Scripts & Models

---
+++ Games/Running & Jumping/RunningAndJumping_KH/Assets/CharacterMovement.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterMovement : MonoBehaviour {
+
+	private CharacterController character;
+	private Vector3 moveVector;
+	public float gravity = 20.0F;
+	public float moveSpeed = 6f;
+	public float jumpPower = 10f;
+
+	void Start (){
+		character = GetComponent<CharacterController> ();
+	}
+
+	void MoveHorizontal (float myInput){
+		moveVector.x = myInput;
+		moveVector = transform.TransformDirection (moveVector * moveSpeed * Time.deltaTime);
+		character.Move (moveVector);
+	}
+
+	void Update () {
+
+		if (character.isGrounded) {
+			moveVector = new Vector3(Input.GetAxis("Horizontal"), 0, 0);
+			moveVector = transform.TransformDirection(moveVector);
+			moveVector *= moveSpeed;
+			if (Input.GetButton("Jump"))
+				moveVector.y = jumpPower;
+
+		}
+			
+		MoveHorizontal (Input.GetAxis ("Horizontal"));
+		moveVector.y -= gravity * Time.deltaTime;
+	}
+}
